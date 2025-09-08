095986772020fdc30fd088cd5d2ea5e3627c9fcf
kurtmodrow <kurt.modrow@getsheeni.com>
2025-09-08 05:09:29 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,15 @@
-export default { reactStrictMode: true };
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'header', key: 'accept', value: 'text/html' }],
+        destination: '/',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
