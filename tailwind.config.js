7a3b33f1aee3ed77ad6dedf9f0ab870d4c095575
kurtmodrow <kurt.modrow@getsheeni.com>
2025-09-08 04:15:18 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: { extend: { colors: { night: "#0B0F1A" } } },
+  plugins: [],
+};
